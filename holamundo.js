afbf5ce37c8eb73eb8e3943f72ba3613122138a4
E301 sede B Tecnologico Comfenalco <E301@Academico.local>
2023-06-21 20:43:26 -0500

    rama de multiplicacion

--- holamundo.js
+++ holamundo.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const holamundo = express();
 
-holamundo.get('/', (req, res) => res.send('Hola Mundo!'));
+holamundo.get('/', (req, res) => {
+    const a = 16;
+    const b = 89
+    const result = a * b
+    res.send(`El resultado de la multiplicacion es ${a} * ${b} es: ${result}`)
+});
 
 module.exports = holamundo;
